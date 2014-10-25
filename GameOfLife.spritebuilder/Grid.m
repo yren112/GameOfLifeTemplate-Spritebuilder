974061c0c6fd8f3a9bb322bd7c5e6fffafc28292
Cheng Ren <Crenberry@Xins-MacBook-Air.local>
2014-10-24 21:23:53 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -76,6 +76,12 @@ static const int GRID_COLUMNS = 10;
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
     //get the row and column that was touched, return the Creature inside the corresponding cell
+    
+    int row, column;
+    
+    row = touchPosition.y / _cellHeight;
+    column = touchPosition.x / _cellWidth;
+    return _gridArray[row][column];
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
